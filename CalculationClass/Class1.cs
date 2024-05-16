251d395bca50d1721d4217ef714b139b254d46cc
Linh Dan Nguyen <89968584+msjuliee@users.noreply.github.com>
2024-05-16 23:22:55 +0700

    Add files via upload

---
+++ CalculationClass/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationClass
+{
+    public class Calculation
+    {
+        public string calculate(float a, float b)
+        {
+            string plus = (a + b).ToString();
+            string minus = (a - b).ToString();
+            string multiply = (a * b).ToString();
+            string divide = (Math.Round((a / b), 2)).ToString();
+
+            return "Result: \n Plus = " + plus + "\n Minus = " + minus + "\n Multiply = " + multiply + "\n Divide = " + divide;
+        }
+    }
+}
